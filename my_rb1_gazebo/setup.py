3d82e88df3ace233ceae8ec0176b78562a503fde
Kyle Schuette <kschuette2008@gmail.com>
2025-09-15 22:31:36 +0000

    Initialized empty_world and spawn launch files. Successful test of empty world launch

--- my_rb1_gazebo/setup.py
+++ my_rb1_gazebo/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'my_rb1_gazebo'
@@ -10,10 +12,13 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*.stl')),
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*.sdf')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='user',
+    maintainer='Kyle Schuette',
     maintainer_email='kschuette2008@gmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
